c7e8f8e045e5861753486cba171a55c62db3dfae
Shawn Tan <shawn@wtf.sg>
2014-03-01 01:44:26 -0800

    Done.

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+
+var script = document.createElement('script');
+script.src = 'https://raw.github.com/shawntan/bookmarklet-toolkit/selector/selector.js';
+var css = document.createElement('link');
+css.rel = 'stylesheet';
+css.href = 'https://raw.github.com/shawntan/bookmarklet-toolkit/selector/bookmarklet.css';
+document.head.appendChild(script);
+document.head.appendChild(css);
+
+// copy to toolbar
+javascript:(function(){var a=document.createElement("script");a.src="https://raw.github.com/shawntan/bookmarklet-toolkit/selector/selector.js";var b=document.createElement("link");b.rel="stylesheet";b.href="https://raw.github.com/shawntan/bookmarklet-toolkit/selector/bookmarklet.css";document.head.appendChild(a);document.head.appendChild(b);})();
